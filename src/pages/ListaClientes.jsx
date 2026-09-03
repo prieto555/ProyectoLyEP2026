68af9550cd88f7e2d44ebe0aa74f5865cf3b1a3b
DanielPalermoo <45328550@fi.unju.edu.ar>
2026-09-03 16:40:30 -0300

    refactor(clientes): centralizar peticiones HTTP en clientesService usando axios

--- src/pages/ListaClientes.jsx
+++ src/pages/ListaClientes.jsx
@@ -2,6 +2,7 @@ import "../css/listaclientes.css"
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FormCliente from "../components/FormCliente";
+import clientesService from "../services/clientesService";
 
 const ListaClientes = () => {
   const [clientes, setClientes] = useState([]);
@@ -10,13 +11,8 @@ const ListaClientes = () => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/users")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Error al obtener clientes");
-        }
-        return res.json();
-      })
+    clientesService
+      .obtenerClientes()
       .then((data) => {
         setClientes(data);
         setLoading(false);
